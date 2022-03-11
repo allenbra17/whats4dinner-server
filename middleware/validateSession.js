356aebbc4d8fda151e4471abd5431187ab664296
allenbra17 <allenbra17@gmail.com>
2022-03-11 16:52:23 -0500

    setting up heroku

--- middleware/validateSession.js
+++ middleware/validateSession.js
@@ -7,7 +7,7 @@ const validateSession = async (req, res, next) => {
             return next()
         } else if (req.headers.authorization) {
             const { authorization } = req.headers
-            const payload = authorization ? token.verify(authorization, process.env.JWT_KEY) : undefined
+            const payload = authorization ? jwt.verify(authorization, process.env.JWT_SECRET) : undefined
             if (payload) {
                 let foundUser = await UserModel.findOne({
                     where: {id: payload.id}
